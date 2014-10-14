4d4cdc606980af2b38d9d11645a4a99cae121c18
Regina Lam <ReginaLamRL@gmail.com>
2014-10-14 17:12:12 -0400

    Added main elements and styles to page

--- js/index.js
+++ js/index.js
@@ -1,36 +1,16 @@
 
-var html = '';
-var posts = [
-	{
-		still: '#',
-		video: '#',
-		quote: '#'
-	}, {
-		still: '#',
-		video: '#',
-		quote: '#'
-	}, {
-		still: '#',
-		video: '#',
-		quote: '#'
-	}, {
-		still: '#',
-		video: '#',
-		quote: '#'
-	}, {
-		still: '#',
-		video: '#',
-		quote: '#'
-	}, {
-		still: '#',
-		video: '#',
-		quote: '#'
-	}, {
-		still: '#',
-		video: '#',
-		quote: '#'
-	}, {
-];
+<script>
+	$(window).scroll(function() {
+		$('h3').each(function(){
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+100) {
+				$(this).addClass("slideUp");
+			}
+		});
+	});
+</script>
 
 
 
