52ebbc63c5f9aa4393ebc4d61a5f582300bbab81
Regina Lam <ReginaLamRL@gmail.com>
2014-10-15 08:31:18 -0400

    Fixes to images, etc.

--- js/index.js
+++ js/index.js
@@ -1,18 +1,6 @@
 
-// Check element type and class (slideLeft vs. slideRight)
-$(window).scroll(function() {
-	$('h3').each(function(){
-	var imagePos = $(this).offset().top;
 
-	var topOfWindow = $(window).scrollTop();
-		if (imagePos < topOfWindow+100) {
-			$(this).addClass("slideLeft");
-		}
-	});
-});
-
-
-$(document).ready(function() {
+	
 	$('.popup-youtube').magnificPopup({
 		disableOn: 700,
 		type: 'iframe',
@@ -22,4 +10,3 @@ $(document).ready(function() {
 
 		fixedContentPos: false
 	});
-});
