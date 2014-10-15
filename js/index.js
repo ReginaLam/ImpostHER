c21958b71da97fb9799fcf0426ad7cae6cc37029
Regina Lam <ReginaLamRL@gmail.com>
2014-10-15 19:03:27 -0400

    stuff

--- js/index.js
+++ js/index.js
@@ -1,12 +1,18 @@
+$(window).scroll(function() {
+	$('h3').each(function(){
+	var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+100) {
+				$(this).addClass("fadeInLeft");
+			}
+		});
+
+ 	});
+});
 
 
-	
-	$('.popup-youtube').magnificPopup({
-		disableOn: 700,
-		type: 'iframe',
-		mainClass: 'mfp-fade',
-		removalDelay: 160,
-		preloader: false,
 
-		fixedContentPos: false
-	});
+
+
+// waypoints.js plug-in set triggers -- 
